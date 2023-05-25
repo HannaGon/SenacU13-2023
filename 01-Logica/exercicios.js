ccc89986988d61953a43b8ca9386664f37d19374
Hanna Gonçalves <109752012+HannaGon@users.noreply.github.com>
2023-05-25 17:07:29 -0300

    Create exercicios.js

---
+++ 01-Logica/exercicios.js
@@ -0,0 +1,15 @@
+//1
+//Crie um pequeno programa que calcule a média
+//entre 4 valores e exibe o resultado
+
+//2
+//Crie um programa que exibe a data de hoje
+//porém o dia e o ano precisam ser números (não string)
+
+//3
+//Crie um programa que exibe seu nome e
+//aniversário, com dia em número (não string)
+
+//4
+//Crie um programa que exibe uma string + um
+//número calculado na hora (Ex.: "comprei x pastéis")
