fa28dc6ed10b6550e635b8ca173bdc183ab50261
Hanna <hanna.goncalves.pro@gmail.com>
2023-05-25 21:33:38 -0300

    exercícios aula 03 adicionados

--- 03-Logica/exercicios.js
+++ 03-Logica/exercicios.js
@@ -1,1 +1,26 @@
-//soon
+//01
+//Crie uma função que soma 3 valores e tira a média
+
+//02
+//Crie uma função que concatena 2 strings e 1 número
+
+//03
+//Reescreva a função do exercício 01 como arrow function
+
+//04
+//Reescreva a função do exercício 02 como arrow function
+
+//05
+//Crie um objeto com 1 a 3 dados diferentes e exiba o
+//valor de um ou mais desses dados como desejar
+
+//06
+//Crie um objeto com 3 a 5 dados diferentes e exiba o
+//valor de um ou mais desses dados como desejar
+
+//07
+//Desestruture ao menos 2 valores de um dos objetos
+//criados nos exercícios 05 ou 06 e exiba eles separadamente
+
+//08
+//utilize um objeto Date() para exibir a data atual
