1510df8b452d3b64bf5dafee8252e527590da67b
pxulor@gmail.com <suporte@uept03-02-03.servens.lan>
2023-05-30 20:16:44 -0300

    exercicios resolvidos

---
+++ 01-Logica/murilo.js
@@ -0,0 +1,27 @@
+//Exercicio 1
+console.log("---ATIVIDADE 1---")
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+console.log(`A média é de: ${(numero1 + numero2 + numero3 + numero4) / 4}`)
+
+//Exercicio 2
+console.log("\n---ATIVIDADE 2---")
+let dia = 30
+let mes = "maio"
+let ano = 2023
+console.log(`Hoje é dia: ${dia} de ${mes} de ${ano}`)
+
+//Exercicio 3
+console.log("\n---ATIVIDADE 3---")
+let nome = "Murilo"
+let diaAniversario = 19
+let mesAniversario = "maio"
+console.log(`${nome} fez aniversário em ${diaAniversario} de ${mesAniversario}`)
+
+//Exercicio 4
+console.log("\n---ATIVIDADE 4---")
+let x = "comprei "
+let y = " pasteis"
+console.log(x + (1+1) + y)
