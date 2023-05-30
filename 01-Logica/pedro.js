e873b3531abf8a33706376e0b2d7d1b556d26554
ppedro <pp220466@gmail.com>
2023-05-30 20:14:40 -0300

    exercicios resolvidos

---
+++ 01-Logica/pedro.js
@@ -0,0 +1,23 @@
+console.log("Exercício 1\n")
+let number1 = 2
+let number2 = 5
+let number3 = 12
+let number4 = 7
+
+console.log(`media= ${(number1 + number2 + number3 + number4) / 4}\n`)
+
+console.log("Exercício 2\n")
+let dia = 30, ano = 2023, mes = "maio" 
+console.log(`Hoje é dia ${dia} de ${mes} de ${ano}\n`)
+
+console.log("Exercício 3\n")
+
+let nome = "pedro"
+dia = 16
+
+console.log(`${nome }, fez aniversário em ${dia}/${mes}/${ano}`)
+
+console.log("\nExercóco4\n")
+
+console.log(`Foram comprados ${18 * 67} pasteis`)
+
